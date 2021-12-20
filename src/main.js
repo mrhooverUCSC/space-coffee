a81fcd9653885513519bda9efa037c0901f89ee0
matthooverslac <61717155+mrhooverUCSC@users.noreply.github.com>
2021-12-19 16:18:58 -0800

    Cafe Tab Complete

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-let credits = 0;
+let credits = 10;
 let debt = 0;
 let coffeeBeans = 0;
 let coffee = 0;
 let flour = 0;
+let butter = 0;
+let sugar = 0;
+let chocolate = 0;
+let eggs = 0;
 let cupcakes = 0;
+let croissants = 0;
 
 function borrowCredits(){
     increaseCredits(1);
@@ -48,15 +53,55 @@ function buyFlour(){
         increaseFlour(1);
     }
 }
+function buyButter(){
+    if(credits >= 1){
+        increaseCredits(-1);
+        increaseButter(1);
+    }
+}
+function buySugar(){
+    if(credits >= 1){
+        increaseCredits(-1);
+        increaseSugar(1);
+    }
+}
+function buyChocolate(){
+    if(credits >= 1){
+        increaseCredits(-1);
+        increaseChocolate(1);
+    }
+}
+function buyEggs(){
+    if(credits >= 1){
+        increaseCredits(-1);
+        increaseEggs(1);
+    }
+}
 function makeCupcakes(){
-    if(flour >= 2){
+    if(flour >= 2, eggs >= 2, chocolate >= 1){
         increaseFlour(-2);
+        increaseEggs(-2);
+        increaseChocolate(-1);
         increaseCupcakes(1);
+    }
+}
+function makeCroissant(){
+    if(flour >= 2, butter >= 2, sugar >= 1){
+        increaseFlour(-2);
+        increaseButter(-2);
+        increaseSugar(-1);
+        increaseCroissants(1);
     }
 }
 function sellCupcakes(){
     if(cupcakes >= 1){
         increaseCupcakes(-1);
+        increaseCredits(10);
+    }
+}
+function sellCroissants(){
+    if(croissants >= 1){
+        increaseCroissants(-1);
         increaseCredits(10);
     }
 }
@@ -89,20 +134,85 @@ function increaseFlour(increase){
     flour += increase;
     document.getElementById("flour").innerHTML = "Flour: " + Math.floor(flour);
 }
+function increaseButter(increase){
+    butter += increase;
+    document.getElementById("butter").innerHTML = "Butter: " + Math.floor(butter);
+}
+function increaseSugar(increase){
+    sugar += increase;
+    document.getElementById("sugar").innerHTML = "Sugar: " + Math.floor(sugar);
+}
+function increaseChocolate(increase){
+    chocolate += increase;
+    document.getElementById("chocolate").innerHTML = "Chocolate: " + Math.floor(chocolate);
+}
+function increaseEggs(increase){
+    eggs += increase;
+    document.getElementById("eggs").innerHTML = "Eggs: " + Math.floor(eggs);
+}
 function increaseCupcakes(increase){
     cupcakes += increase;
     document.getElementById("cupcakes").innerHTML = "Cupcakes: " + Math.floor(cupcakes);
 }
+function increaseCroissants(increase){
+    croissants += increase;
+    document.getElementById("croissants").innerHTML = "Croissants: " + Math.floor(croissants);
+}
 
 
-window.setInterval(function(){ //tick every .1 seconds
-    if(credits==0){
-        document.getElementById("clicker").hidden = false;
+function paneChange(name){
+    if(name == 'cafe'){
+        document.getElementById("cafePane").style.display = "block";
+        document.getElementById("managementPane").style.display = "none";
+        document.getElementById("contractPane").style.display = "none";
+        document.getElementById("recipesPane").style.display = "none";
+        document.getElementById("selectCafe").className = "paneSelector selected";
+        document.getElementById("selectManagement").className = "paneSelector";
+        document.getElementById("selectContracts").className = "paneSelector";
+        document.getElementById("selectRecipes").className = "paneSelector";
+    }
+    else if(name == 'management'){
+        document.getElementById("cafePane").style.display = "none";
+        document.getElementById("managementPane").style.display = "block";
+        document.getElementById("contractPane").style.display = "none";
+        document.getElementById("recipesPane").style.display = "none";
+        document.getElementById("selectCafe").className = "paneSelector";
+        document.getElementById("selectManagement").className = "paneSelector selected";
+        document.getElementById("selectContracts").className = "paneSelector";
+        document.getElementById("selectRecipes").className = "paneSelector";
+    }
+    else if(name == 'contracts'){
+        document.getElementById("cafePane").style.display = "none";
+        document.getElementById("managementPane").style.display = "none";
+        document.getElementById("contractPane").style.display = "block";
+        document.getElementById("recipesPane").style.display = "none";
+        document.getElementById("selectCafe").className = "paneSelector";
+        document.getElementById("selectManagement").className = "paneSelector";
+        document.getElementById("selectContracts").className = "paneSelector selected";
+        document.getElementById("selectRecipes").className = "paneSelector";
     }
     else{
+        document.getElementById("cafePane").style.display = "none";
+        document.getElementById("managementPane").style.display = "none";
+        document.getElementById("contractPane").style.display = "none";
+        document.getElementById("recipesPane").style.display = "block";
+        document.getElementById("selectCafe").className = "paneSelector";
+        document.getElementById("selectManagement").className = "paneSelector";
+        document.getElementById("selectContracts").className = "paneSelector";
+        document.getElementById("selectRecipes").className = "paneSelector selected";
+    }
+}
+
+window.setInterval(function(){ //tick every .1 seconds
+    /*
+    if(credits==0 && document.getElementById("cafePane").style.display == "block"){
+        document.getElementById("clicker").hidden = false;
+    }
+    else if(document.getElementById("cafePane").style.display == "block"){
         document.getElementById("clicker").hidden = true;
     }
     buildingsTick();
     contractsTick();
     upgradesCheck();
+    */
 }, 100);
